442fffed3ad0a8f187c244fda1184354c31aca51
Akif Gumussu <info@aquive.nl>
2021-02-09 19:25:01 +0000

    Implemented realy basic delete command

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# magerun2-delete-products
-Magerun2 module to delete products from a Magento 2 store 
+# Magerun2 delete products module
+[Magerun 2](https://github.com/netz98/n98-magerun2/) module to delete products from a Magento 2 store 
+
+Installation
+------------
+
+1. Clone in the n98-magerun modules folder
+
+        cd ~/.n98-magerun2/modules/ && git clone https://github.com/AquiveMedia/magerun2-delete-products.git aquivemedia-addons
+
+Commands
+--------
+
+```bash
+$ magerun2 products:delete
+```
+Warning
+--------
+With great power comes great responsibility. This is my first (experimental) magerun module. It is untested. Try to use it on a development(!) environment only.
+
+Todo
+--------
+- prompt to ask if the user is sure to proceed
+- (performance) tests
+
+Possible features to implement
+--------
+
+- delete based on product id list
+- delete based on product sku list
---
+++ src/AquiveMedia/ProductsDeleteCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AquiveMedia;
+
+use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ProductsDeleteCommand extends AbstractMagentoCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('products:delete')            
+            ->setDescription('WARNING! Deletes all products from store')
+        ;
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output);
+        if ($this->initMagento()) {
+            
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $objectManager->get('Magento\Framework\Registry')->register('isSecureArea', true);
+            $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+            $collection = $productCollection->create()->load();
+            $appState = $objectManager->get('\Magento\Framework\App\State');
+            $appState->setAreaCode('frontend');
+                        
+            foreach ($collection as $product){
+                try {
+                    $output->writeln('Deleted SKU: '. $product->getSku());
+                    $product->delete();
+            
+                } catch (Exception $e) {
+                    $output->writeln('Unable to delete product SKU: ' . $product->getSku());
+                    $output->writeln($e->getMessage());
+                }   
+            }      
+                                                                         
+        }
+    }
+}
